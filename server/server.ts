4050811bdd323dd7c6796aa4c054198dc98401a5
Marwan <2017159@brunel.ac.uk>
2025-12-23 14:41:42 +0000

    Connected to Mongo Database

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,22 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
 
-const PORT = parseInt(process.env.PORT || "5000", 10);
-const NODE_ENV = process.env.NODE_ENV ?? "development";
+dotenv.config();
+
+const PORT = parseInt(process.env.PORT, 10);
+const NODE_ENV = process.env.NODE_ENV;
 
 const app = express();
 
 app.use(express.json());
+
+connectDB();
+
+// Routes
+app.get("/", (req: Request, res: Response) => {
+  res.send("API IS RUNNING...");
+});
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`);
